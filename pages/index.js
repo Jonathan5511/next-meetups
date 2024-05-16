0075b7b558a8bd50ca55a3e52546e508e803c595
Jonathan5511 <jonathanpereira5511@gmail.com>
2024-05-16 19:09:52 +0530

    Adding meta data

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,20 @@
 import MeetupList from "../components/meetups/MeetupList";
 import { MongoClient } from "mongodb";
-
-const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    title: "A First Meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/800px-Stadtbild_M%C3%BCnchen.jpg?20130611211153",
-    address: "Some address 5, 12345 Some City",
-    description: "This is a second meetup!",
-  },
-];
+import Head from "next/head";
+import { Fragment } from "react";
 
 const HomePage = (props) => {
-  return <MeetupList meetups={props.meetups} />;
+  return;
+  <Fragment>
+    <Head>
+      <title>React Meetups</title>
+      <meta
+        name="description"
+        content="Browse a huge list of highly active React meetups!"
+      />
+    </Head>
+    <MeetupList meetups={props.meetups} />;
+  </Fragment>;
 };
 
 // export async function getServerSideProps(context) {
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,5 +1,7 @@
 import { useRouter } from "next/router";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+import { Fragment } from "react";
+import Head from "next/head";
 
 const NewMeetupPage = () => {
   const router = useRouter();
@@ -16,7 +18,18 @@ const NewMeetupPage = () => {
     console.log(data);
     router.push("/");
   };
-  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>Add a new Meetup</title>
+        <meta
+          name="description"
+          content="Add your own meetups and create amazing networking opportunities"
+        />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+    </Fragment>
+  );
 };
 
 export default NewMeetupPage;
